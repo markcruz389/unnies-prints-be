c9807f3a950e2d2176ae0fc0300b14be8b48cc9f
markcruz389 <markanthonycruz1230@gmail.com>
2023-10-10 23:47:52 +0800

    chore: adds function that inserts test user in DB

--- src/auth/auth.spec.ts
+++ src/auth/auth.spec.ts
@@ -3,19 +3,32 @@ import dotenv from 'dotenv';
 
 import { mongoConnect, monngoDisconnect } from '../_services/mongo';
 import initializeApp from '../_services/app';
+import { checkIfUserExists, signUpUser } from '../users/users.model';
 
 dotenv.config();
 
 const config = {
     MONGO_URL: process.env.MONGO_URL || '',
-    TEST_ACCOUNT_USERNAME: process.env.TEST_ACCOUNT_USERNAME,
-    TEST_ACCOUNT_PASSWORD: process.env.TEST_ACCOUNT_PASSWORD,
+    TEST_ACCOUNT_USERNAME: process.env.TEST_ACCOUNT_USERNAME || '',
+    TEST_ACCOUNT_PASSWORD: process.env.TEST_ACCOUNT_PASSWORD || '',
 };
 const app = initializeApp();
+
+// Function created for github actions workflow
+const createTestUser = async (username: string, password: string) => {
+    const exists = await checkIfUserExists(username);
+    if (!exists) {
+        await signUpUser(username, password);
+    }
+};
 
 describe('Auth APIs', () => {
     beforeAll(async () => {
         await mongoConnect(config.MONGO_URL);
+        await createTestUser(
+            config.TEST_ACCOUNT_USERNAME,
+            config.TEST_ACCOUNT_PASSWORD
+        );
     });
     afterAll(async () => {
         await monngoDisconnect();
